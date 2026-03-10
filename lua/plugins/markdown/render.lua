b931d4f6ac0fdd646ebb108badbc4cfba93ba5cb
Kumamoto Seita <135863869+9mada4@users.noreply.github.com>
2026-03-11 01:10:47 +0900

    chore(nvim): Neovim設定を更新 markview.nvimを導入　背景が右まで出るのが微妙

--- lua/plugins/markdown/render.lua
+++ lua/plugins/markdown/render.lua
@@ -12,64 +12,78 @@ return {
   },
 
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown" },
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown", "quarto", "rmd" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "echasnovski/mini.nvim",
     },
-    config = function()
-      require("render-markdown").setup({
-        code = {
-            width = 'block',
-            left_pad = 2,
-            right_pad = 4,
-        },
-        heading = {
-            enabled = true,
-            sign = false,
-            icons = { "󰼏 ", "󰎨 ", "󰼑 ", "󰎲 ", "󰼓 ", "󰎴 " },
-            position = 'overlay',
-            width = 'block',
-            left_margin = 0,
-            left_pad = 0,
-            right_pad = 4,
-            min_width = 0,
-            border = false,
-            border_virtual = false,
-            border_prefix = false,
-            -- above = '▄',
-            -- below = '▀',
-            above = '_',
-            below = '‾',
-            backgrounds = {
-                'RenderMarkdownH1Bg',
-                'RenderMarkdownH2Bg',
-                'RenderMarkdownH3Bg',
-                'RenderMarkdownH4Bg',
-                'RenderMarkdownH5Bg',
-                'RenderMarkdownH6Bg',
-            },
-            -- backgrounds = {},
-            foregrounds = {
-                'RenderMarkdownH1',
-                'RenderMarkdownH2',
-                'RenderMarkdownH3',
-                'RenderMarkdownH4',
-                'RenderMarkdownH5',
-                'RenderMarkdownH6',
-            },
-            custom = {},
-         },
-      })
-
-      -- -- 1. 背景色Aを定義
-      -- -- vim.api.nvim_set_hl(0, "MarkdownCodeBlockBg", { fg = "fg", bg = "#40383E" })
-      vim.api.nvim_set_hl(0, "MarkdownCodeBlockBg", { link = "@markup.raw.markdown_inline" })
-      -- -- 2.1. コードブロック本文に背景色Aを当てる
-      vim.api.nvim_set_hl(0, "markdownCodeBlock", { link = "MarkdownCodeBlockBg" })
-      -- -- 2.2. コードフェンスの```などにも背景色Aを当てる
-      vim.api.nvim_set_hl(0, "markdownCodeDelimiter", { link = "MarkdownCodeBlockBg" })
-    end,
+    opts = {
+      preview = {
+        icon_provider = "mini",
+      },
+    },
   },
+
+  -- {
+  --   "MeanderingProgrammer/render-markdown.nvim",
+  --   ft = { "markdown" },
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "echasnovski/mini.nvim",
+  --   },
+  --   config = function()
+  --     require("render-markdown").setup({
+  --       code = {
+  --           width = 'block',
+  --           left_pad = 2,
+  --           right_pad = 4,
+  --       },
+  --       heading = {
+  --           enabled = true,
+  --           sign = false,
+  --           icons = { "󰼏 ", "󰎨 ", "󰼑 ", "󰎲 ", "󰼓 ", "󰎴 " },
+  --           position = 'overlay',
+  --           width = 'block',
+  --           left_margin = 0,
+  --           left_pad = 0,
+  --           right_pad = 4,
+  --           min_width = 0,
+  --           border = false,
+  --           border_virtual = false,
+  --           border_prefix = false,
+  --           -- above = '▄',
+  --           -- below = '▀',
+  --           above = '_',
+  --           below = '‾',
+  --           backgrounds = {
+  --               'RenderMarkdownH1Bg',
+  --               'RenderMarkdownH2Bg',
+  --               'RenderMarkdownH3Bg',
+  --               'RenderMarkdownH4Bg',
+  --               'RenderMarkdownH5Bg',
+  --               'RenderMarkdownH6Bg',
+  --           },
+  --           -- backgrounds = {},
+  --           foregrounds = {
+  --               'RenderMarkdownH1',
+  --               'RenderMarkdownH2',
+  --               'RenderMarkdownH3',
+  --               'RenderMarkdownH4',
+  --               'RenderMarkdownH5',
+  --               'RenderMarkdownH6',
+  --           },
+  --           custom = {},
+  --        },
+  --     })
+  --
+  --     -- -- 1. 背景色Aを定義
+  --     -- -- vim.api.nvim_set_hl(0, "MarkdownCodeBlockBg", { fg = "fg", bg = "#40383E" })
+  --     vim.api.nvim_set_hl(0, "MarkdownCodeBlockBg", { link = "@markup.raw.markdown_inline" })
+  --     -- -- 2.1. コードブロック本文に背景色Aを当てる
+  --     vim.api.nvim_set_hl(0, "markdownCodeBlock", { link = "MarkdownCodeBlockBg" })
+  --     -- -- 2.2. コードフェンスの```などにも背景色Aを当てる
+  --     vim.api.nvim_set_hl(0, "markdownCodeDelimiter", { link = "MarkdownCodeBlockBg" })
+  --   end,
+  -- },
 }
