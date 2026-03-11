d963cb9db04ebc122ac0c2bd6c419ca61c1fe51d
Kumamoto Seita <135863869+9mada4@users.noreply.github.com>
2026-03-12 01:53:45 +0900

    Glowプレビューをリッチに

--- lua/plugins/markdown/preview.lua
+++ lua/plugins/markdown/preview.lua
@@ -4,7 +4,18 @@ return {
     ft = { "markdown" },
     cmd = { "Glow" },
     config = function()
-      require("glow").setup({})
+      if vim.env.NO_COLOR ~= nil then
+        vim.env.NO_COLOR = nil
+      end
+
+      if vim.env.TERM == nil or vim.env.TERM == "" or vim.env.TERM == "dumb" then
+        vim.env.TERM = "xterm-256color"
+      end
+
+      require("glow").setup({
+        style = "dracula",
+        pager = true,
+      })
     end,
   },
 }
