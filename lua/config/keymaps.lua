b844399ef9695a4487e5b7315e246a7201d72a5c
9mada4 <skumamoto@ikunolab.net>
2026-04-17 10:31:46 +0900

    chore(nvim): Neovim設定を更新

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -284,11 +284,28 @@ vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
 -- LazyGit
 -- =========================
 
+local function ensure_lazygit_color_env()
+  if vim.fn.has("mac") ~= 1 then
+    return
+  end
+
+  if vim.env.NO_COLOR ~= nil then
+    vim.env.NO_COLOR = nil
+  end
+  if vim.env.TERM == nil or vim.env.TERM == "" or vim.env.TERM == "dumb" then
+    vim.env.TERM = "xterm-256color"
+  end
+  if vim.env.COLORTERM == nil or vim.env.COLORTERM == "" then
+    vim.env.COLORTERM = "truecolor"
+  end
+end
+
 -- Space gg : lazygit UI を開く
 vim.keymap.set("n", "<leader>gg", function()
   if vim.fn.executable("lazygit") ~= 1 then
     vim.notify("'lazygit' is not installed or not on PATH", vim.log.levels.WARN)
     return
   end
+  ensure_lazygit_color_env()
   vim.cmd("LazyGit")
 end, { desc = "Open LazyGit" })
--- lua/plugins/editor/lazygit.lua
+++ lua/plugins/editor/lazygit.lua
@@ -1,3 +1,19 @@
+local function ensure_lazygit_color_env()
+  if vim.fn.has("mac") ~= 1 then
+    return
+  end
+
+  if vim.env.NO_COLOR ~= nil then
+    vim.env.NO_COLOR = nil
+  end
+  if vim.env.TERM == nil or vim.env.TERM == "" or vim.env.TERM == "dumb" then
+    vim.env.TERM = "xterm-256color"
+  end
+  if vim.env.COLORTERM == nil or vim.env.COLORTERM == "" then
+    vim.env.COLORTERM = "truecolor"
+  end
+end
+
 return {
   {
     "kdheepak/lazygit.nvim",
@@ -8,5 +24,8 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      ensure_lazygit_color_env()
+    end,
   },
 }
